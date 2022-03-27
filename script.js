d2047112db10ba1eba947b72fd0d270833be9e6c
aseto1 <102394392+aseto1@users.noreply.github.com>
2022-03-26 17:50:18 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function red() {
+    document.body.style.backgroundImage = "url('orangebb.png')";
+
+    document.body.style.backgroundPosition = "absolute";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundSize = "contain";
+    //document.body.style.backgroundPosition = "center";
+    document.body.style.width = "100%";
+    document.body.style.height = "550vh";
+    document.body.style.objectFit = "contain";
+    document.body.style.marginTop = "10px";
+    document.body.style.backgroundSize = "100%100%";
